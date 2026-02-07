ce8d490072df4ee2e492890fea70769063d83464
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 09:51:35 +0530

    Create Exercise class for workout tracking

---
+++ Exercise.java
@@ -0,0 +1,13 @@
+package miniproject;
+import java.util.*;
+public class Exercise {
+	
+	    String name;
+	    int duration; 
+	    int calories;
+
+	    Exercise(String name, int duration, int rate) {
+	        this.name = name;
+	        this.duration = duration;
+	        this.calories = duration * rate;
+	    }}
